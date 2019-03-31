277777f3d02f240f69f060fd9623d2dd005a8bd9
Ohyeon Kwon <37339357+blueeads@users.noreply.github.com>
2019-04-01 02:11:35 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 # Blog_TeamProject-with-daeun-
-
-SNS-style blogs that recommend movies or dramas by imitating tumblr's format
-
-Using the database through php
-
-Development period: 2019.03.25
-
-Web Designer: Daeun Lee
-front & back end developer : Onyeon Kwon
--- main.php
+++ main.php
@@ -3,6 +3,10 @@
 ?>
 
 <style>
+.mainbox {
+    top : 200px;
+    margin : auto 20%;
+}
 .top-line {
     width : 100%;
     height : 50px;
@@ -14,11 +18,24 @@
     padding-right : 20px;
 
 }
-.main-logo {
-    width : 20%;
-    position : relative;
 
+.main-logo {
+    width : 20%
 }
+
+.main-logo img {
+    text-align : center;
+}
+
+.main-logo .img-button {
+    width : 100%;
+    margin-left : 15%;
+    position : relative;
+    background: url("./logo.png") no-repeat;
+    border: none;
+    cursor: pointer;
+}
+
 .top-menu {
     width : 40%;    
     position: absolute;
@@ -32,11 +49,110 @@
     margin: 0;
     padding: 0;
 }
+
+.view-box {
+    float : left;
+    margin-top : 60px;
+    width : 65%;
+}
+
+.view-box ul {
+    width : 100%;
+}
+
+.view-box li {
+    width : 100%;
+}
+
+.view-title {
+    background : white;
+    height : 50px;
+    font-size : 24px;
+}
+
+.view-title p {
+    margin : 0;
+    padding : 10px;
+}
+
+.view-image {
+    max-height : 770px;
+    max-width : 100%;
+    /* background : yellow; */
+    height : auto;
+    width : auto;
+    padding : 0;
+}
+
+.view-image img {
+    width : 100%;
+    height : 100%;
+}
+
+.view-star {
+    background : white;
+    padding : 10px 0;
+}
+
+.view-star p {
+    margin : 0;
+    padding-left : 10px;
+}
+
+.view-recommend {
+    background : white;
+    height : 150px;
+}
+
+.view-recommend p {
+    margin : 0;
+    padding-left : 10px;
+}
+
+.view-user {
+    background : white;
+    padding-bottom : 10px;
+}
+
+.view-user p {
+    margin : 0;
+    padding-left : 10px;
+}
+
+.movie-bar {
+    width : 300px;
+    color : white;
+    margin-top : 50px;
+}
+
+.movie-bar p {
+    border-bottom : 2px solid rgb(0, 173, 181);
+    padding : 10px;
+    font-size : 20px;
+}
+
+.movie-bar li {
+    color : white;
+    border-bottom : 1px solid gray;
+    padding-top : 8px;
+    padding-bottom : 8px;
+}
+
+.movie-bar a {
+    padding-left : 10px;
+    color : white;
+}
+
+.right-sidebox {
+    float : right;
+}
 </style>
+
 
 <div class = "top-line">
     <div class = "main-logo">
-    <button type = "button" onclick = "location.href = './'"> <img src="logo.png" alt="" width = "40x" height = "40px"> </button>
+        <img src="logo.png" alt="" width = "45px" height = "45px">
+    <input type="button" class="img-button">
     </div>
     <div class = "top-menu">
         <ul>
@@ -47,8 +163,31 @@
         </ul>
     </div>
 </div>
-
+<div class = "mainbox">
+    <div>
+        <div class = "view-box">
+            <ul>
+                <li class = "view-title"><p>SKY캐슬</p></li>
+                <li class = "view-image"><img src="sky.jpg" alt="스카이캐슬"></li>
+                <li class = "view-star"><p>★★★★☆</p></li>
+                <li class = "view-recommend"><p>SKY캐슬은 ...</p></li>
+                <li class = "view-user"><p>작성자 : 피라미드</p></li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class = "right-sidebox">
+        <div class = "movie-bar"><p>영화</p>
+        <ul>
+            <li><a href="http://">로맨스</a></li>
+            <li><a href="http://">액션</a></li>
+            <li><a href="http://">공포</a></li>
+            <li><a href="http://">판타지</a></li>
+        </ul>
+    </div>
+</div>
+</div>
 
 <?php
     require __DIR__ . '/footer.php';
-?>
+    ?>
